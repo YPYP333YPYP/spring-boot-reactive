12fee7c8614aef84c6bc31ab1d8fc31428ce8259
YPYP333YPYP <ypyp333ypyp@gmail.com>
2025-03-28 00:39:49 +0900

    feat: Product SQL 스크립트 작성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,3 +6,17 @@ CREATE TABLE IF NOT EXISTS warehouses
     description TEXT
 );
 
+CREATE TABLE IF NOT EXISTS products
+(
+    id SERIAL PRIMARY KEY,
+    product_name VARCHAR(255) NOT NULL,
+    unit VARCHAR(50) NOT NULL,
+    price NUMERIC(10, 2) NOT NULL,
+    category VARCHAR(100),
+    supplier VARCHAR(255),
+    current_stock INTEGER NOT NULL DEFAULT 0,
+    threshold_level INTEGER NOT NULL DEFAULT 0,
+    last_used_at TIMESTAMP,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    );
