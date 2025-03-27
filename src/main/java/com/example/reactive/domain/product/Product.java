31e818c6a1f418d3e23f7f0f38f2a5567f5737ed
YPYP333YPYP <ypyp333ypyp@gmail.com>
2025-03-28 00:39:41 +0900

    feat: Product 엔티티 생성

---
+++ src/main/java/com/example/reactive/domain/product/Product.java
@@ -0,0 +1,32 @@
+package com.example.reactive.domain.product;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table("products")
+public class Product {
+    @Id
+    private Long id;
+
+    @Column("product_name")
+    private String name;           // 제품 이름
+    private String unit;           // 단위
+    private Double price;          // 가격
+    private String category;       // 카테고리
+    private String supplier;       // 공급업체
+    private Integer currentStock;  // 현재 재고수량
+    private Integer thresholdLevel; // 임계량
+    private LocalDateTime lastUsedAt; // 최근 사용일
+    private LocalDateTime createdAt; // 생성일
+    private LocalDateTime updatedAt; // 수정일
+}
